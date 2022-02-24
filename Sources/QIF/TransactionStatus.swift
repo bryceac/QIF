c664ecfaa5f901e0cf67f8183990e632946f4d1b
Bryce Campbell <tonyhawk2100@gmail.com>
2022-02-24 07:41:12 -0800

    add enumeration to read status of transaction.

---
+++ Sources/QIF/TransactionStatus.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Bryce Campbell on 2/24/22.
+//
+
+import Foundation
+
+enum TransactionStatus: String, CaseIterable {
+    case cleared = "X", reconciled = "*"
+}
