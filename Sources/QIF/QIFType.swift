c367eefab21a634d2d7b6a571e1ec9bead13c586
Bryce Campbell <tonyhawk2100@gmail.com>
2022-02-24 07:42:19 -0800

    add type enumerations for account types.

---
+++ Sources/QIF/QIFType.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Bryce Campbell on 2/24/22.
+//
+
+import Foundation
+
+enum QIFType: String, CaseIterable {
+    case cash = "Cash", bank = "Bank", creditCard = "CCard", liability = "Oth L", asset = "Oth A"
+}
