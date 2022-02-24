7a4bd29e37e840de3db310053db0aaf8cfaed5d3
Bryce Campbell <tonyhawk2100@gmail.com>
2022-02-24 08:30:39 -0800

    fix content in sample QIF file.

--- Tests/QIFTests/QIFTests.swift
+++ Tests/QIFTests/QIFTests.swift
@@ -35,7 +35,7 @@ final class QIFTests: XCTestCase {
     func parseQIFString() {
         let samHill = QIFTransaction(date: Date(), checkNumber: 1260, vendor: "Sam Hill Credit Union", address: "Sam Hill Credit Union", amount: 500, category: "Opening Balance", memo: "Open Account", status: .cleared)
 
-        let fakeStreetElectronics = QIFTransaction(date: Date(), checkNumber: nil, vendor: "Fake Street Electronics", address: "Fake street Electronics", amount: 200, category: "Gifts", memo: "Head set", status: nil)
+        let fakeStreetElectronics = QIFTransaction(date: Date(), checkNumber: nil, vendor: "Fake Street Electronics", address: "Fake street Electronics", amount: -200, category: "Gifts", memo: "Head set", status: nil)
 
         let velociraptorEntertainment = QIFTransaction(date: Date(), checkNumber: nil, vendor: "Velociraptor Entertainment", address: "Velociraptor Entertainment", amount: 50000, category: nil, memo: "Pay Day", status: nil)
         
