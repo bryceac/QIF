c0085872d52772080ee5c9fe00e9c6d6d9b9742b
Bryce Campbell <tonyhawk2100@gmail.com>
2022-02-24 08:11:10 -0800

    try to make QIF LosslessConvertible.

--- Sources/QIF/QIFType.swift
+++ Sources/QIF/QIFType.swift
@@ -10,3 +10,16 @@ import Foundation
 enum QIFType: String, CaseIterable {
     case cash = "Cash", bank = "Bank", creditCard = "CCard", liability = "Oth L", asset = "Oth A"
 }
+
+extension QIFType: LosslessStringConvertible {
+    var description: String {
+        return self.rawValue
+    }
+    
+    init?(_ description: String) {
+        guard let typeMatches = description.matching(regexPattern: "!Type:(.*)"), let firstMatch = typeMatches.first else { return nil }
+        
+        self.init(rawValue: firstMatch[1])
+    }
+    
+}
