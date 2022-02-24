1959be434f744a45287f41f5d70fada20871f518
Bryce Campbell <tonyhawk2100@gmail.com>
2022-02-24 10:02:34 -0800

    add type to deal with parsing errors.

---
+++ Sources/QIF/Enumerations/Errors/QIFParseError.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Bryce Campbell on 2/24/22.
+//
+
+import Foundation
+
+public enum QIFParseError: LocalizedError {
+    case fileUnreadable, incorrect
+}
