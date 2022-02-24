180eb66c52c2855c89b0f6a6992d2ca48a5c7f6d
Bryce Campbell <tonyhawk2100@gmail.com>
2022-02-24 07:53:48 -0800

    create first test.

--- Tests/QIFTests/QIFTests.swift
+++ Tests/QIFTests/QIFTests.swift
@@ -2,10 +2,23 @@ import XCTest
 @testable import QIF
 
 final class QIFTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(QIF().text, "Hello, World!")
+    func parseTransaction() {
+        let transactionText = """
+        D\(Date())
+        T500
+        C*
+        N1260
+        PSam Hill Credit Union
+        MOpen Account
+        ASam Hill Credit Union
+        LOpening Balance
+        ^
+        """
+        
+        let samHill = QIFTransaction(date: Date(), checkNumber: 1260, vendor: "Sam Hill Credit Union", address: "Sam Hill Credit Union", amount: 500, category: "Opening Balance", memo: "Open Account", status: .cleared)
+        
+        if let parsedTransaction = QIFTransaction(transactionText) {
+            XCTAssertEqual(parsedTransaction, samHill)
+        }
     }
 }
