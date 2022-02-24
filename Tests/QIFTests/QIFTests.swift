15b6dfb8e45d085bd9995a7561177d92721504f0
Bryce Campbell <tonyhawk2100@gmail.com>
2022-02-24 11:02:22 -0800

    make test function throwable.

--- Tests/QIFTests/QIFTests.swift
+++ Tests/QIFTests/QIFTests.swift
@@ -99,4 +99,28 @@ final class QIFTests: XCTestCase {
         
         XCTAssertNoThrow(try qif.save(to: DOCUMENTS_DIRECTORY.appendingPathComponent("test").appendingPathExtension("qif")))
     }
+    
+    func readFile() throws {
+        let samHill = QIFTransaction(date: Date(), checkNumber: 1260, vendor: "Sam Hill Credit Union", address: "Sam Hill Credit Union", amount: 500, category: "Opening Balance", memo: "Open Account", status: .cleared)
+
+        let fakeStreetElectronics = QIFTransaction(date: Date(), checkNumber: nil, vendor: "Fake Street Electronics", address: "Fake street Electronics", amount: -200, category: "Gifts", memo: "Head set", status: nil)
+
+        let velociraptorEntertainment = QIFTransaction(date: Date(), checkNumber: nil, vendor: "Velociraptor Entertainment", address: "Velociraptor Entertainment", amount: 50000, category: nil, memo: "Pay Day", status: nil)
+        
+        let type = QIFType.bank
+        
+        let qif = QIF(type: type, transactions: [
+            samHill,
+            fakeStreetElectronics,
+            velociraptorEntertainment
+        ])
+        
+        let DOCUMENTS_DIRECTORY = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        let testFile = DOCUMENTS_DIRECTORY.appendingPathComponent("test").appendingPathExtension("qif")
+        
+        if let parsedQIF = try QIF.load(from: testFile) {
+            XCTAssertEqual(parsedQIF, qif)
+        }
+    }
 }
