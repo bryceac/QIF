a9c5491aa1e350e07f8861bf4e5a97d3048f7abd
Bryce Campbell <tonyhawk2100@gmail.com>
2022-03-03 11:33:20 -0800

    remove lossless string convertible from type enumeration.

--- Sources/QIF/Enumerations/QIFType.swift
+++ Sources/QIF/Enumerations/QIFType.swift
@@ -10,16 +10,3 @@ import Foundation
 public enum QIFType: String, CaseIterable {
     case cash = "Cash", bank = "Bank", creditCard = "CCard", liability = "Oth L", asset = "Oth A"
 }
-
-extension QIFType: LosslessStringConvertible {
-    public var description: String {
-        return self.rawValue
-    }
-    
-    public init?(_ description: String) {
-        guard let typeMatches = description.matching(regexPattern: "!Type:(.*)"), let firstMatch = typeMatches.first else { return nil }
-        
-        self.init(rawValue: firstMatch[1])
-    }
-    
-}
