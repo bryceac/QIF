1e47bba1354fab3d7d95fdec0a2b0b77cbd27e81
Bryce Campbell <tonyhawk2100@gmail.com>
2022-03-03 11:54:51 -0800

    work on creating error for deal with QIF types.

--- Sources/QIF/Enumerations/Errors/QIFTypeParsingError.swift
+++ Sources/QIF/Enumerations/Errors/QIFTypeParsingError.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+enum QIFTypeParsingError: LocalizedError {
+    case incorrectFormat, invalidType
+    
+    var errorDescription: String? {
+        var error = ""
+        
+        switch self {
+        case .incorrectFormat: error = "The format presented in invalid. The type or section must be specified like in this format !Type:Account Type"
+        case .invalidType: error = "Specified Type is invalid. The type can only be Cash, Bank, CCard, Oth L, or Oth A"
+        }
+        
+        return error
+    }
+}
