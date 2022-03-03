abe03c15e72c13f11d09f32b0374ef8e1573c541
Bryce Campbell <tonyhawk2100@gmail.com>
2022-03-03 11:47:34 -0800

    create throwing initializer for QIF.

--- Sources/QIF/Enumerations/QIFType.swift
+++ Sources/QIF/Enumerations/QIFType.swift
@@ -10,3 +10,9 @@ import Foundation
 public enum QIFType: String, CaseIterable {
     case cash = "Cash", bank = "Bank", creditCard = "CCard", liability = "Oth L", asset = "Oth A"
 }
+
+extension QIFType {
+    init(_ text: String) throws {
+        
+    }
+}
