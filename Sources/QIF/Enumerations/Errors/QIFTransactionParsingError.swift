27f8661132ab4dd6dd679d48e75df008ef93c8b4
Bryce Campbell <tonyhawk2100@gmail.com>
2022-03-04 10:04:22 -0800

    keep working on errors to produce.

--- Sources/QIF/Enumerations/Errors/QIFTransactionParsingError.swift
+++ Sources/QIF/Enumerations/Errors/QIFTransactionParsingError.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 enum QIFTransactionParsingError: LocalizedError {
-    case noDateFound, wrongDateFormat, valueNotNumerical(value: String, field: String)
+    case noDateFound, wrongDateFormat, valueNotNumerical(value: String, field: String), fieldNotFound(field: String)
     
     var errorDescription: String? {
         var error = ""
@@ -17,6 +17,7 @@ enum QIFTransactionParsingError: LocalizedError {
         case .noDateFound: error = "Date could not be found"
         case .wrongDateFormat: error = "Date must be in MM/DD/YYYY format"
         case .valueNotNumerical(let value, let field): error = "\(value) is an invalid value for \(field)"
+        case .fieldNotFound(let field): error = "The field \(field) could not be found"
         }
         
         return error
