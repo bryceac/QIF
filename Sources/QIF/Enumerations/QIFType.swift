27e4781ae31cf514d8f5b7f7e5a72ca7b95e676a
Bryce Campbell <tonyhawk2100@gmail.com>
2022-03-04 15:09:51 -0800

    remove custom initializer for enumeration.

--- Sources/QIF/Enumerations/QIFType.swift
+++ Sources/QIF/Enumerations/QIFType.swift
@@ -10,17 +10,3 @@ import Foundation
 public enum QIFType: String, CaseIterable {
     case cash = "Cash", bank = "Bank", creditCard = "CCard", liability = "Oth L", asset = "Oth A"
 }
-
-extension QIFType {
-    init(_ text: String) throws {
-        guard let typeMatches = text.matching(regexPattern: "!Type:(.*)"), let firstMatch = typeMatches.first else {
-            throw QIFTypeParsingError.incorrectFormat
-        }
-                
-        guard let type = QIFType(rawValue: firstMatch[1]) else {
-            throw QIFTypeParsingError.invalidType
-        }
-        
-        self = type
-    }
-}
