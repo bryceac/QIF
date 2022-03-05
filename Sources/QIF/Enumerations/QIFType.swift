445809978ffdfd3e916d3f33fb66827f0fbb3e2a
Bryce Campbell <tonyhawk2100@gmail.com>
2022-03-04 16:39:45 -0800

    try working on different way to grab type.

--- Sources/QIF/Enumerations/QIFType.swift
+++ Sources/QIF/Enumerations/QIFType.swift
@@ -10,3 +10,15 @@ import Foundation
 public enum QIFType: String, CaseIterable {
     case cash = "Cash", bank = "Bank", creditCard = "CCard", liability = "Oth L", asset = "Oth A"
 }
+
+extension QIFType {
+    init(_ text: String) throws {
+        guard let sectionMatches = text.matching(regexPattern: "!Type:(.*)") else {
+            throw QIFTypeParsingError.incorrectFormat
+        }
+        
+        guard let type = sectionMatches.compactMap({ $0.last }).first(where: { !(.none ~= QIFType(rawValue: $0)) }) else {
+            
+        }
+    }
+}
