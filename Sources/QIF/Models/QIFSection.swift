d7239e639d3e8636301ed09f1a50573ea7d7ae52
Bryce Campbell <tonyhawk2100@gmail.com>
2022-03-05 13:28:05 -0800

    add a type to deal with sections.

---
+++ Sources/QIF/Models/QIFSection.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by Bryce Campbell on 3/5/22.
+//
+
+import Foundation
+
+struct QIFSection {
+    let type: QIFType
+    
+    var transactions: Set<QIFTransaction>
+}
+
+extension QIFSection {
+    init(_ text: String) throws {
+        type = try QIFType(text)
+        
+        let transaction = try QIFTransaction(text)
+        
+        transactions = Set([transaction])
+    }
+}
