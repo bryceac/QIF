3d1ff55d33eb8cd81f0d81a9a636eb5754193c25
Bryce Campbell <tonyhawk2100@gmail.com>
2022-03-06 10:48:02 -0800

    remove unneeded regex initialization.

--- Sources/QIF/Enumerations/QIFType.swift
+++ Sources/QIF/Enumerations/QIFType.swift
@@ -10,22 +10,3 @@ import Foundation
 public enum QIFType: String, CaseIterable {
     case cash = "Cash", bank = "Bank", creditCard = "CCard", liability = "Oth L", asset = "Oth A"
 }
-
-extension QIFType {
-    /**
-    initialize type of QIF file from string.
-    - Returns: QIFType enumeration value.
-    - Throws: QIFTypeParsingError if the format is not correct or section name is not valid.
-    */
-    public init(_ text: String) throws {
-        guard let sectionMatches = text.matching(regexPattern: "!Type:(.*)") else {
-            throw QIFTypeParsingError.incorrectFormat
-        }
-        
-        guard let typeString = sectionMatches.compactMap({ $0.last }).first(where: { !(.none ~= QIFType(rawValue: $0)) }), let type = QIFType(rawValue: typeString) else {
-            throw QIFTypeParsingError.noValidSections
-        }
-        
-        self = type
-    }
-}
