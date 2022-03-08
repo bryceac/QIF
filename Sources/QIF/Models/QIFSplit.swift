6dbec495bda0beb1e3abf6440225d51a9c20359e
Bryce Campbell <tonyhawk2100@gmail.com>
2022-03-07 16:53:25 -0800

    add type to represent a split.

---
+++ Sources/QIF/Models/QIFSplit.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Bryce Campbell on 3/7/22.
+//
+
+import Foundation
+
+public struct QIFSplit {
+    public var category: String?
+    public var memo: String
+    public var amount: Double
+}
